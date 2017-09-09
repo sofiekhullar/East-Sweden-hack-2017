cff3ed3a995fad48cb5e02c7c91e83558255b756
Joakim Konac <joako217@student.liu.se>
2017-09-09 14:20:14 +0200

    add non functional css and background image

--- solceller/App.js
+++ solceller/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, ScrollView, Image } from 'react-native';
+import ButtonComponent, { CircleButton, RoundButton, RectangleButton } from 'react-native-button-component';
+import { Container, Header, Content, Button, Text, Thumbnail } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+//import Button from 'apsl-react-native-button'
+//import CircleButton from 'react-native-circle-button';
+//import Button from 'react-native-button';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+
+        <Container>
+                <Image
+                    source={require('./images/background.png')}
+                    style={styles.imageThings}>
+            <Header />
+            <Content>
+                <Grid>
+
+                    <Col style={{ backgroundColor: '#635DB7', height: 500 }}>
+                    </Col>
+                </Grid>
+                <Grid>
+                    <Col style={{ backgroundColor: '#635DB7', height: 0 }}></Col>
+                </Grid>
+
+            </Content>
+                    <Button block success large >
+                        <Text>Nästa</Text>
+                    </Button>
+        </Image>
+        </Container>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -20,4 +44,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    boxContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    mainContentContainer: {
+      flex: 7,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+      flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageThings: {
+        flex: 1,
+        width: undefined,
+        height: undefined,
+        backgroundColor:'transparent',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
 });
+
+/*
+return (
+    <Container>
+        <View style={styles.container}>
+            <View style={[styles.boxContainer, styles.mainContentContainer]}>
+            </View>
+            <View style={[styles.boxContainer, styles.buttonContainer]}>
+
+                <Button block success>
+                    <Text>Success</Text>
+                </Button>
+            </View>
+        </View>
+
+    </Container>
+);*/
