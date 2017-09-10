03f2fd3d74d1a05eba7633d747bd31b72542260b
sofiekhullar <sofie_k@live.se>
2017-09-10 10:34:00 +0200

    step4view

--- solceller/Views/Step4View.js
+++ solceller/Views/Step4View.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Image} from 'react-native';
+import Button from 'apsl-react-native-button'
 
 class Step4View extends React.Component{
     constructor(props){
         super(props);
     }
+
+
+    navigate() {
+        this.props.navigation.navigate('Result');
+    }
+
     render(){
         return(
-            <View>
-                <Text/>
-                <Text/>
-                <Text/>
-                <Text>This is Step4 View</Text>
-                <Button onPress={() => this.props.navigation.navigate('Result', { name: 'Jane' })}
-                        title="Press here!">
-                </Button>
+            <View style={styles.container}>
+                <Image source={require('../images/background.png')} style={styles.imageThings} >
+
+                    <View style={[styles.boxContainer, styles.mainContentContainer]}>
+                        <Text style={styles.stepTextStyle}>Step 4</Text>
+                        <Text style={styles.descriptionTextStyle}>How many square meters?</Text>
+                        <TextInput
+                            onChangeText={(inputText) => this.setState({inputText})}
+                            // Adding hint in Text Input using Place holder.
+                            placeholder="Fyll i här..."
+
+                            // Making the Under line Transparent.
+                            underlineColorAndroid='transparent'
+
+                            // Calling the custom TextInputStyleClass.
+                            style={styles.TextInputStyleClass}/>
+                    </View>
+
+                    <View style={[styles.boxContainer, styles.buttonContainer]}>
+                        <Button style={styles.buttonStyle} textStyle={styles.textStyle} onPress={()=> this.navigate()}>
+                            <Text style={styles.buttonTextStyle}>Nästa</Text>
+                        </Button>
+                    </View>
+                </Image>
             </View>
         )
     };
@@ -26,3 +49,67 @@ Step4View.navigationOptions = {
 };
 
 export default Step4View;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+
+    },
+    boxContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    mainContentContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
+    imageThings: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: null,
+        height: null,
+        backgroundColor: 'rgba(0,0,0,0)'
+    },
+    stepTextStyle: {
+        color: 'white',
+        fontFamily: 'Helvetica',
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingBottom: 10
+    },
+    descriptionTextStyle: {
+        color: 'white',
+        fontFamily: 'Helvetica',
+        fontSize: 20,
+        paddingBottom: 60
+    },
+    buttonTextStyle: {
+        color: 'white',
+        fontFamily: 'Helvetica',
+        fontSize: 20
+    },
+    buttonStyle: {
+        backgroundColor: 'green',
+        borderColor: 'green',
+        width: 350,
+        height: 60
+    },
+    TextInputStyleClass: {
+        textAlign: 'center',
+        height: 50,
+        width: 300,
+        borderWidth: 2,
+        borderColor: 'green',
+        borderRadius: 10 ,
+        backgroundColor : "#FFFFFF"
+    }
+});
